f382a0f9a785a78be22e99fddb9c83877038f09a
etoolabode <etoolabode@gmail.com>
2024-07-04 07:58:33 +0100

    ...mobile nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+"use client";
 import Image from "next/image";
 import Navbar from "./shared/components/navbar/navbar";
 
 export default function Home() {
   return (
     <main className="relative">
-      <section className="w-full -mt-24 2xl:-mt-40 min-h-[100vh] bg-herobg1 bg-cover py-10 px-4">
+      <section className="w-full -mt-[6.5rem] 2xl:-mt-40 min-h-[100vh] bg-herobg1 bg-cover py-10 px-4">
         <div className="flex flex-col lg:w-11/12 2xl:w-10/12 m-auto lg:flex-row lg:m-h-[50vh] lg:mt-48 2xl:mt-64">
           <div className="min-h-40 mb-20 lg:mb-0 mt-28 flex flex-col justify-center items-center lg:items-start lg:justify-start lg:mt-0 w-full lg:w-6/12">
             <h2 className="py-1 text-4xl lg:text-5xl xl:text-7xl 2xl:text-[6.5rem] text-white font-creato font-extralight uppercase mb-1">
--- app/shared/components/navbar/navbar.tsx
+++ app/shared/components/navbar/navbar.tsx
@@ -1,25 +1,90 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PrimaryBtn from "../buttons/primary-btn";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default function Navbar() {
+export default function Navbar({ setMobileVisibility }: any) {
   const [selectedTab, setselectedTab] = useState("Home");
+  const [isOpen, setisOpen] = useState(false);
 
   const tabsData = [
-    { id: 1, title: "Home", path: "/" },
-    { id: 2, title: "About Us", path: "/about" },
+    {
+      id: 1,
+      title: "Home",
+      path: "/",
+      icon: (
+        <>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9 16.9999H15M3 14.5999V12.1301C3 10.9814 3 10.407 3.14805 9.87807C3.2792 9.40953 3.49473 8.96886 3.78405 8.57768C4.11067 8.13608 4.56404 7.78346 5.47078 7.07822L8.07078 5.056C9.47608 3.96298 10.1787 3.41648 10.9546 3.2064C11.6392 3.02104 12.3608 3.02104 13.0454 3.2064C13.8213 3.41648 14.5239 3.96299 15.9292 5.056L18.5292 7.07822C19.436 7.78346 19.8893 8.13608 20.2159 8.57768C20.5053 8.96886 20.7208 9.40953 20.8519 9.87807C21 10.407 21 10.9814 21 12.1301V14.5999C21 16.8401 21 17.9603 20.564 18.8159C20.1805 19.5685 19.5686 20.1805 18.816 20.564C17.9603 20.9999 16.8402 20.9999 14.6 20.9999H9.4C7.15979 20.9999 6.03969 20.9999 5.18404 20.564C4.43139 20.1805 3.81947 19.5685 3.43597 18.8159C3 17.9603 3 16.8401 3 14.5999Z"
+              stroke="#036E03"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      title: "About Us",
+      path: "/about",
+      icon: (
+        <>
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.5 20.366V18.366C13.5 15.6045 11.2614 13.366 8.5 13.366C5.73858 13.366 3.5 15.6045 3.5 18.366V20.366H13.5ZM13.5 20.366H21.5V19.366C21.5 16.4204 19.2614 14.366 16.5 14.366C15.0867 14.366 13.8103 14.9915 12.9009 15.9971M11.5 7.36597C11.5 9.02282 10.1569 10.366 8.5 10.366C6.84315 10.366 5.5 9.02282 5.5 7.36597C5.5 5.70911 6.84315 4.36597 8.5 4.36597C10.1569 4.36597 11.5 5.70911 11.5 7.36597ZM18.5 9.36597C18.5 10.4705 17.6046 11.366 16.5 11.366C15.3954 11.366 14.5 10.4705 14.5 9.36597C14.5 8.2614 15.3954 7.36597 16.5 7.36597C17.6046 7.36597 18.5 8.2614 18.5 9.36597Z"
+              stroke="#036E03"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </>
+      ),
+    },
     { id: 3, title: "Fleet", path: "/fleet" },
     { id: 4, title: "Park", path: "/park" },
     { id: 5, title: "Agency", path: "/agency" },
     { id: 6, title: "Traveler’s Club", path: "/Travelers-Club" },
   ];
 
+  const dialogRef: any = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (isOpen) {
+        document.body.classList.add("no-scroll");
+      } else {
+        document.body.classList.remove("no-scroll");
+      }
+    };
+
+    handleScroll();
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isOpen]);
+
   return (
     <div className="w-full p-4 sticky top-2 z-50">
-      <div className="w-full lg:w-11/12 2xl:w-10/12 m-auto bg-white rounded-lg px-4 py-2 2xl:py-6 2xl:px-6 xl:max-w-8xl">
+      <div className="w-full lg:w-11/12 2xl:w-10/12 m-auto bg-white rounded-lg px-0 py-2 2xl:py-6 2xl:px-6 xl:max-w-8xl -mt-3 lg:-mt-0">
         {/* desktop navbar */}
-        <div className="hidden lg:block py-1">
+        <div className="hidden lg:block py-1 px-4">
           <div className="flex flex-row items-center justify-between">
             <div className="lg:w-1/12 xl:w-2/12">
               <Image
@@ -61,14 +126,108 @@ export default function Navbar() {
         {/* ---------- */}
 
         {/* mobile navbar */}
-        <div className="lg:hidden py-2">
-          <div className="flex flex-row items-center justify-between">
+        <div className="lg:hidden py-2 relative">
+          <div className="flex flex-row items-center justify-between px-4">
             <div>
               <Image src="./logo.svg" width={80} height={35} alt="" />
             </div>
             <div></div>
-            <div></div>
+            <div
+              onClick={() => {
+                setisOpen(!isOpen);
+                if (setMobileVisibility) {
+                  setMobileVisibility(!isOpen);
+                }
+              }}
+            >
+              <MenuIcon />
+            </div>
           </div>
+
+          <dialog
+            open={isOpen}
+            className="bg-white rounded-xl p-3 w-full flex flex-col justify-between  top-16 h-[86vh]"
+            ref={dialogRef}
+          >
+            <div>
+              {tabsData.map((tab, index: number) => (
+                <div
+                  className={`flex items-center gap-1 justify-center py-3 border ${
+                    index > 0 ? "border-t-gray-100 border-l-0 border-r-0" : ""
+                  }`}
+                  key={index}
+                  onClick={() => {
+                    setselectedTab(tab.title);
+                  }}
+                >
+                  {tab.icon && selectedTab == tab.title && (
+                    <div>{tab.icon}</div>
+                  )}
+                  <div className="cursor-pointer">
+                    <span
+                      className={`${
+                        selectedTab == tab.title && "font-bold text-urban-green"
+                      }`}
+                    >
+                      {tab.title}
+                    </span>
+                  </div>
+                </div>
+              ))}
+
+              <div className="mt-4">
+                <div className="flex items-center gap-2 justify-center">
+                  <button className="w-1/2 bg-green-100 rounded py-3">
+                    Login
+                  </button>
+                  <button className="py-3 w-1/2 bg-urban-green text-white rounded">
+                    Register
+                  </button>
+                </div>
+                <div>
+                  <h2 className="text-center mt-6">
+                    <span className="font-bold text-xl">Urban App</span>{" "}
+                    <span className="p-1 italic font-light">
+                      Available on Web and App
+                    </span>
+                  </h2>
+                  <div className="flex justify-center items-center gap-2 mt-2">
+                    <Image
+                      src="/assets/appstoreIcon.svg"
+                      width={140}
+                      height={28}
+                      alt=""
+                      className=""
+                    />
+                    <Image
+                      src="/assets/playstoreIcon.svg"
+                      width={140}
+                      height={28}
+                      alt=""
+                      className=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="rounded-xl bg-urban-black p-3 w-full py-4 flex justify-between">
+              <div className="w-1/2">
+                <Image
+                  src="/assets/mobilefooterLogo.svg"
+                  width={80}
+                  height={28}
+                  alt=""
+                  className=""
+                />
+                <p className="mt-2 italic font-light text-gray-100 text-[0.6rem]">
+                  Urban is an enabler of their inter-state travel needs and
+                  experience
+                </p>
+              </div>
+              <div className="w-1/2"></div>
+            </div>
+          </dialog>
         </div>
 
         {/* ---------- */}
