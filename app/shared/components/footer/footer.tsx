a75cd1654801e55c09b95358910b68d4d1f7bad6
etoolabode <etoolabode@gmail.com>
2024-07-20 13:20:00 +0100

    ...urban mobile footer

--- app/shared/components/footer/footer.tsx
+++ app/shared/components/footer/footer.tsx
@@ -160,6 +160,151 @@ export default function Footer() {
         </div>
 
         {/* ======================================= */}
+
+        {/* ================== mobile footer ===============*/}
+        <div className="px-5 pb-1 -mt-10 lg:hidden">
+          <div className="w-full m-auto">
+            <h2 className="font-creato font-thin italic text-white">
+              Reach out to us
+            </h2>
+            <div className="mt-0">
+              <form className="flex items-center gap-2">
+                <input className="text-gray-600 bg-white rounded-full w-8/12 ooter-input" />
+                <button className="py-4 w-4/12 px-4 rounded-full bg-green-700 text-white mt-2 font-creato text-xs">
+                  Send Now
+                </button>
+              </form>
+              <div className="mt-3 flex px-2 items-center gap-4">
+                <div>
+                  <Image
+                    src="/assets/linkedicon.svg"
+                    width={26}
+                    height={26}
+                    alt=""
+                  />
+                </div>
+
+                <div>
+                  <Image
+                    src="/assets/twittericon.svg"
+                    width={26}
+                    height={26}
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <Image
+                    src="/assets/fbicon.svg"
+                    width={26}
+                    height={26}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full py-4 flex justify-between gap-8 mt-6">
+            <div className="w-1/2">
+              <Image
+                src="/assets/mobilefooterLogo.svg"
+                width={100}
+                height={28}
+                alt=""
+                className=""
+              />
+              <p className="mt-2 italic font-light text-gray-100 text-[0.7rem] w-11/12">
+                Urban is an enabler of their inter-state travel needs and
+                experience
+              </p>
+            </div>
+            <div className="w-1/2">
+              <div className="flex gap-2 mb-4">
+                <Image
+                  src="/assets/MFooterEmailIcon.svg"
+                  width={32}
+                  height={28}
+                  alt=""
+                  className=""
+                />
+                <div className="text-white text-[0.6rem]">
+                  <p>Email</p>
+                  <p>contact@urban.com</p>
+                </div>
+              </div>
+
+              <div className="flex gap-2">
+                <Image
+                  src="/assets/MCallIcon.svg"
+                  width={32}
+                  height={28}
+                  alt=""
+                  className=""
+                />
+                <div className="text-white text-[0.6rem]">
+                  <p>Call Us</p>
+                  <p>(00) 112 365 489</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* -----links---------- */}
+          <div className="flex flex-col gap-y-10 text-white mt-4">
+            <div className="flex justify-between w-full">
+              <div className="w-1/2">
+                <h2 className="font-creato">Growth & Partnership</h2>
+                <ul className="text-xs xl:text-sm font-thin flex flex-col gap-2 mt-2">
+                  <li>Fleets</li>
+                  <li>Parks</li>
+                  <li>Agencies</li>
+                  <li>Traveler's Club</li>
+                  <li>Traveler’s Kits</li>
+                  <li>Hotels</li>
+                  <li>Urban card</li>
+                </ul>
+              </div>
+              <div className="w-1/2">
+                <h2 className="font-creato">About Us</h2>
+                <ul className="text-xs xl:text-sm font-thin flex flex-col gap-2 mt-2">
+                  <li>Why Urban</li>
+                  <li>Urban Experience</li>
+                  <li>Our Vision</li>
+                  <li>Core Values</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex justify-between w-full">
+              <div className="w-1/2">
+                <h2 className="font-creato">Location</h2>
+                <ul className="text-xs xl:text-sm font-thin flex flex-col gap-2 mt-2">
+                  <li>Abuja</li>
+                  <li>Jos</li>
+                  <li>Makurdi</li>
+                  <li>Minna</li>
+                  <li>View more</li>
+                </ul>
+              </div>
+              <div className="w-1/2">
+                <h2 className="font-creato">Policies</h2>
+                <ul className="text-xs xl:text-sm font-thin flex flex-col gap-2 mt-2">
+                  <li>Terms Of Use</li>
+                  <li>Refund Policy</li>
+                  <li>Behavior Policy</li>
+                  <li>Trip Cancellation Policy</li>
+                  <li>Charter Terms</li>
+                  <li>Hotels T&C</li>
+                  <li>Cards T&C</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="py-2 text-center text-white mt-10 text-sm font-creato font-thin">
+            © Urban {currentYear}. All Rights Reserved.
+          </div>
+        </div>
       </div>
     </footer>
   );
