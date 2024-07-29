8bceddb91fef2230533c278d5ecaaeda2784d67e
Olabode <etoolabode@gmail.com>
2024-07-29 20:41:15 +0100

    ...

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ export default function Aboutpage() {
   }, []);
   return (
     <main className="relative ">
-      <section className="w-full -mt-[7.8rem] 2xl:-mt-40 min-h-[55vh] bg-heroAbout bg-cover py-10 px-4 ">
+      <section className="w-full -mt-[7.8rem] 2xl:-mt-40 min-h-[55vh] lg:min-h-[58vh] bg-heroAbout bg-cover py-10 px-4 ">
         <div className="flex flex-col lg:w-11/12 2xl:w-10/12 m-auto lg:flex-row lg:m-h-[50vh] lg:mt-40 2xl:mt-60"></div>
       </section>
 
