dbfbd4132c2e88ff722a8635296f29ff1bbed385
Olabode <etoolabode@gmail.com>
2024-08-02 12:30:25 +0100

    ...

--- app/booking/(bookings)/passenger-details/page.tsx
+++ app/booking/(bookings)/passenger-details/page.tsx
@@ -6,13 +6,17 @@ import "./passenger-details.scss";
 import Footer from "@/app/shared/components/footer/footer";
 import { useRouter } from "next/navigation";
 import PassengerAccordion from "./passenger-accordion/passenger-accordion";
+import Select from "@mui/joy/Select";
+import { KeyboardArrowDown } from "@mui/icons-material";
+import Option from "@mui/joy/Option";
+import Input from "@mui/joy/Input";
 
 export default function PassengerDetails() {
   const router = useRouter();
   return (
     <>
       <main className="-mt-[7.8rem] 2xl:-mt-40 min-h-[55vh] lg:min-h-[80vh] py-10 px-4">
-        <section className="m-h-96  w-11/12 lg:w-11/12 lg:px-6 m-auto pb-20 2xl:w-10/12 passenger-details-trips-page">
+        <section className="m-h-96  w-full lg:w-11/12 lg:px-6 m-auto pb-20 2xl:w-10/12 passenger-details-trips-page">
           <div className="w-full flex flex-col lg:flex-row">
             <div className="w-full lg:w-1/2">
               <div className="flex items-center gap-x-2">
@@ -24,7 +28,145 @@ export default function PassengerDetails() {
                 </span>
               </div>
               <div className="mt-4">
-                <PassengerAccordion />
+                <PassengerAccordion
+                  sn={1}
+                  passengerName={"Tade Ogunbade"}
+                  seatNumber="B1"
+                >
+                  <div className="w-full flex flex-col gap-10">
+                    <div className=" w-full mt-6">
+                      <label className="text-base font-light">Title</label>
+                      <div className="mt-3">
+                        <Select
+                          defaultValue={18}
+                          indicator={<KeyboardArrowDown />}
+                          sx={{ height: "48px", fontSize: "0.8rem" }}
+                          slotProps={{
+                            listbox: {
+                              sx: {
+                                maxHeight: "200px",
+                              },
+                            },
+                          }}
+                        >
+                          {["Mr", "Mrs", "Miss", "Sir"].map(
+                            (seat, index: number) => (
+                              <Option
+                                value={seat}
+                                sx={{ fontSize: "0.8rem" }}
+                                key={index}
+                              >
+                                {seat}
+                              </Option>
+                            )
+                          )}
+                        </Select>
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label className="text-base font-light">First Name</label>
+                      <div className="mt-3">
+                        <Input
+                          type="text"
+                          placeholder="Type in here…"
+                          sx={{
+                            height: "46px",
+                            fontSize: "0.8rem",
+                            "--Input-focusedInset": "var(--any, )",
+                            "--Input-focusedThickness": "0.25px",
+                            "--Input-focusedHighlight": "#15560c",
+                            "&:focus-outside": {
+                              borderColor: "#000000",
+                            },
+                            ":focus": "#000",
+                          }}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label className="text-base font-light">Last Name</label>
+                      <div className="mt-3">
+                        <Input
+                          type="text"
+                          placeholder="Type in here…"
+                          sx={{
+                            height: "46px",
+                            fontSize: "0.8rem",
+                            "--Input-focusedInset": "var(--any, )",
+                            "--Input-focusedThickness": "0.25px",
+                            "--Input-focusedHighlight": "#15560c",
+                            "&:focus-outside": {
+                              borderColor: "#000000",
+                            },
+                            ":focus": "#000",
+                          }}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="w-full">
+                      <label className="text-base font-light">
+                        Phone Number
+                      </label>
+                      <div className="mt-3">
+                        <Input
+                          type="text"
+                          placeholder="Type in here…"
+                          sx={{
+                            height: "46px",
+                            fontSize: "0.8rem",
+                            "--Input-focusedInset": "var(--any, )",
+                            "--Input-focusedThickness": "0.25px",
+                            "--Input-focusedHighlight": "#15560c",
+                            "&:focus-outside": {
+                              borderColor: "#000000",
+                            },
+                            ":focus": "#000",
+                          }}
+                        />
+                      </div>
+                    </div>
+
+                    <div className=" w-full mt-2">
+                      <div className="w-full flex items-center justify-between">
+                        <label className="text-base font-light">
+                          Select Seat
+                        </label>
+                        <button className="bg-green-100 text-urban-green py-2 px-4 text-xs rounded-lg">
+                          See seat Arrangement
+                        </button>
+                      </div>
+                      <div className="mt-3">
+                        <Select
+                          defaultValue={18}
+                          indicator={<KeyboardArrowDown />}
+                          sx={{ height: "48px", fontSize: "0.8rem" }}
+                          slotProps={{
+                            listbox: {
+                              sx: {
+                                maxHeight: "200px",
+                              },
+                            },
+                          }}
+                        >
+                          {["A1", "A2", "B1", "B2", "B3", "C1"].map(
+                            (seat, index: number) => (
+                              <Option
+                                value={seat}
+                                sx={{ fontSize: "0.8rem" }}
+                                key={index}
+                              >
+                                {seat}
+                              </Option>
+                            )
+                          )}
+                        </Select>
+                      </div>
+                    </div>
+                  </div>
+                </PassengerAccordion>
               </div>
             </div>
             {/* ----- right side------ */}
--- app/booking/(bookings)/passenger-details/passenger-accordion/passenger-accordion.tsx
+++ app/booking/(bookings)/passenger-details/passenger-accordion/passenger-accordion.tsx
@@ -3,32 +3,42 @@ import React, { useState } from "react";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-export default function PassengerAccordion() {
+type PropT = {
+  sn: number;
+  passengerName: String;
+  seatNumber: any;
+};
+export default function PassengerAccordion({
+  children,
+  sn,
+  passengerName,
+  seatNumber,
+}: any) {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <div className="w-full p-2 boxShadow">
-      <div className="flex items-center justify-between border border-gray-200 p-2">
-        <div className="text-lg text-urban-green w-3/12">
-          1 Passenger’s info
+    <div className="w-full p-2 boxShadow px-1 lg:px-4">
+      <div
+        className="flex items-center justify-between border border-gray-200 p-2 cursor-pointer"
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <div className="text-base lg:text-lg text-urban-green w-3/12">
+          {sn} Passenger’s info
         </div>
-        <div className="h-8 border border-gray-400"></div>
+        <div className="h-8 border-[0.5px] border-gray-400"></div>
         <div className="flex items-center justify-between w-5/12">
-          <span className="text-sm text-gray-400">Tade Ogunbade</span>
+          <span className="text-sm font-light text-gray-400">
+            {passengerName}
+          </span>
           <span className="text-sm text-gray-400">
             {" "}
-            Seat No: <span className="text-urban-black">B1</span>
+            Seat No: <span className="text-urban-black">{seatNumber}</span>
           </span>
         </div>
-        <div
-          className="cursor-pointer"
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
+        <div className="cursor-pointer">
           {isExpanded ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
         </div>
       </div>
-      {isExpanded && (
-        <div className="min-h-60 border border-red-400 mt-4"></div>
-      )}
+      {isExpanded && <div className="min-h-60">{children}</div>}
     </div>
   );
 }
