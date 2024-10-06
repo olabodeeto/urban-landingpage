6eae447d339fa8674917a308cb6ef3e44c378f61
Olabode <etoolabode@gmail.com>
2024-10-06 17:04:44 +0100

    card pages

--- app/urban-card/page.tsx
+++ app/urban-card/page.tsx
@@ -15,6 +15,7 @@ import "swiper/css/pagination";
 import "./urban-card.scss";
 import AppAd from "../shared/components/app-ad/app-ad";
 import TravelersDialog from "../shared/components/travelers-dialog/travelers-dialog";
+import Link from "next/link";
 
 export default function UrbanCardPage() {
   const [ismovileNavOpen, setismovileNavOpen] = useState(false);
@@ -136,6 +137,155 @@ export default function UrbanCardPage() {
             </div>
           </section>
         </div>
+
+        {/* ----------- ad ----------- */}
+        <div className="flex justify-center items-center py-10 mb-10 mt-20">
+          <section className="w-11/12 lg:w-11/12 px-1 lg:px-6 m-auto lg:mt-4 2xl:w-10/12">
+            <div className="flex flex-col lg:flex-row justify-between">
+              <div className="w-full lg:w-5/12 bg-urban-black p-10 lg:px-1 flex flex-col items-center justify-center gap-4 lg:gap-10 lg:min-h-96">
+                <div className="flex items-center gap-4 text-white w-full md:px-10">
+                  <Image
+                    src="/assets/cardblob1.svg"
+                    width={140}
+                    height={140}
+                    alt=""
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-once="true"
+                  />
+                  <Image
+                    src="/assets/cardblob2.svg"
+                    width={140}
+                    height={140}
+                    alt=""
+                    data-aos="fade-up"
+                    data-aos-duration="1000"
+                    data-aos-once="true"
+                  />
+                </div>
+
+                <div className="mt-4 lg:mt-2 w-full xl:px-1 flex justify-center">
+                  <div className="w-full md:px-10">
+                    <h2
+                      className=" text-2xl font-light lg:text-3xl xl:text-4xl w-10/12 text-white font-creato"
+                      data-aos="fade-up"
+                      data-aos-duration="1800"
+                    >
+                      Spend Smarter with
+                    </h2>
+                    <h2 className=" text-2xl font-bold lg:text-3xl xl:text-4xl w-10/12 text-urban-lightGreen font-creato">
+                      Urban Cards
+                    </h2>
+                  </div>
+                </div>
+
+                <div className="w-full flex justify-center">
+                  <Image
+                    src="/assets/simplecard1.png"
+                    width={120}
+                    height={120}
+                    alt=""
+                    className=" w-8/12"
+                    data-aos="flip-up"
+                    data-aos-duration="1000"
+                  />
+                </div>
+              </div>
+
+              <div className="w-full min-h-[20rem] lg:min-h-[40rem] lg:w-7/12 flex justify-center items-center gap-2 px-2 mt-16 lg:mt-0 xl:p-10">
+                <div className="xl:w-10/12 m-auto">
+                  <h2 className="italic text-3xl  xl:text-5xl font-creato font-thin mb-2">
+                    What would you like to do with{" "}
+                    <span className="text-urban-green font-bold">Urban?</span>
+                  </h2>
+                  <div className="mt-10 mb-4 px-4">
+                    <ul className="flex-col gap-4 text-xl list-item list-decimal font-creato list-outside font-light">
+                      <li className="mb-4">
+                        Enjoy unrivaled discounts across our merchants
+                      </li>
+                      <li className="mb-4">Book your own trip</li>
+                      <li className="mb-4">
+                        Book trips for employees and Execs
+                      </li>
+                      <li className="mb-4">
+                        Manage company cards and expenses
+                      </li>
+                      <li className="mb-4">Create a company travel program</li>
+                      <li className="mb-4">
+                        Attend Urban events across the country
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="p-4 mt-10 flex flex-col gap-6">
+                    <hr />
+                    <li className="text-xl xl:text-2xl font-light italic">
+                      All discounts are for Urban card holders - learn more on
+                      each card with the links below
+                    </li>
+                  </div>
+                  <div className=" flex items-center gap-x-4 mt-4">
+                    <Link href="/personal-card">
+                      <div className="flex cursor-pointerx items-center">
+                        <span className="text-xl font-creato block font-bold">
+                          Personal Card page
+                        </span>
+                        <img src="./assets/roundArrow.svg" alt="" />
+                      </div>
+                    </Link>
+                    <Link href="/corporate-card">
+                      <div className="flex cursor-pointerx items-center">
+                        <span className="text-xl font-creato font-bold">
+                          Corporate Card page
+                        </span>
+                        <img src="./assets/roundArrow.svg" alt="" />
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+        {/* -------------------------- */}
+
+        {/* ===== random ============== */}
+        <section className="min-h-[65vh] bg-urban-green w-full mb-10 mt-10 pl-0 pr-0 pt-10 relative">
+          <div className="w-full lg:w-4/12 m-auto  mt-10 mb-14">
+            <p className="text-4xl xl:text-4xl text-center font-creato font-bold italic text-white">
+              <span className="font-light">Do more</span> with Urban Merchants
+            </p>
+          </div>
+          <div className="w-full flex flex-col lg:flex-row justify-between px-10 gap-y-4 items-center mb-14">
+            <div className="w-[40vh] h-[40vh] lg:w-[20vw] lg:h-[20vw] rounded-full overflow-hidden">
+              <img src="/assets/rand1.png" alt="" className="object-cover" />
+            </div>
+            <div className="w-[40vh] h-[40vh] lg:w-[20vw] lg:h-[20vw] rounded-full overflow-hidden">
+              <img src="/assets/rand2.png" alt="" className="object-cover" />
+            </div>
+            <div className="w-[40vh] h-[40vh] lg:w-[20vw] lg:h-[20vw] rounded-full overflow-hidden">
+              <img src="/assets/rand3.png" alt="" className="object-cover" />
+            </div>
+            <div className="w-[40vh] h-[40vh] lg:w-[20vw] lg:h-[20vw] rounded-full overflow-hidden">
+              <img src="/assets/rand4.png" alt="" className="object-cover" />
+            </div>
+          </div>
+
+          <div className="h-40 overflow-hidden w-full relative">
+            <div className="flex justify-center mt-10 pb-6">
+              <button className="py-3 px-6 2xl:py-4 bg-[#1A1A1A] text-white rounded-lg">
+                Explore our Merchants
+              </button>
+            </div>
+            <Image
+              src="/assets/hiddencard.png"
+              width={120}
+              height={120}
+              alt=""
+              className="w-80 absolute -left-40 -top-2 bottom-0"
+            />
+          </div>
+        </section>
+        {/* =========================== */}
 
         {/* ========================= */}
         <section className="w-full py-10 lg:py-0 lg:w-8/12 2xl:w-10/12 m-auto xl:mt-20 lg:xl:my-0 lg:pb-20 relative card-slider-section">
